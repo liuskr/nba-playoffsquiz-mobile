ed0249637a6584e998bebfa77be17a49c0ded5b8
liuskr <liuskr2017@163.com>
2022-03-17 13:06:27 +0800

    style

--- src/utils/wx.ts
+++ src/utils/wx.ts
@@ -5,7 +5,7 @@ import { getWeChatShare } from '@apis'
 
 export const getWechatShare = async (): Promise<void> => {
   if (isWeiXin()) {
-    const url = `https://nba75th.ihyx.net/newyear/#/`
+    const url = ''
     const { data } = await getWeChatShare({ url })
     wx.config({
       appId: 'wx6b8c3d7f8236e99a',
@@ -17,10 +17,10 @@ export const getWechatShare = async (): Promise<void> => {
 
     wx.ready(() => {
       wx.updateAppMessageShareData({
-        title: 'NBA新春限量版微信红包封面', // 分享标题
-        desc: 'NBA虎年限量版微信红包封面，数量有限，先到先得!', // 分享描述
+        title: '', // 分享标题
+        desc: '!', // 分享描述
         link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: 'https://nba75th-1307342770.cos.ap-guangzhou.myqcloud.com/loading/newyeahshare.jpg', // 分享图标
+        imgUrl: '', // 分享图标
         success: function () {
           // 设置成功
           console.log('share success')
@@ -28,10 +28,10 @@ export const getWechatShare = async (): Promise<void> => {
       })
 
       wx.updateTimelineShareData({
-        title: 'NBA新春限量版微信红包封面', // 分享标题
-        desc: 'NBA虎年限量版微信红包封面，数量有限，先到先得!', // 分享描述
+        title: '', // 分享标题
+        desc: '', // 分享描述
         link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: 'https://nba75th-1307342770.cos.ap-guangzhou.myqcloud.com/loading/newyeahshare.jpg', // 分享图标
+        imgUrl: '', // 分享图标
         success: function () {
           // 设置成功
           console.log('share success')
