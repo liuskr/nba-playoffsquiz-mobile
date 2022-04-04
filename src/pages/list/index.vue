1284d1881270b2f3176ccc4f449a8b58289c91da
liuskr <liuskr2017@163.com>
2022-04-04 14:34:16 +0800

    feat:地址

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -54,21 +54,53 @@
       <div class="footer-fixed-info">
         <div class="footer-fixed-info-points">12312</div>
         <div class="footer-fixed-info-ranking">123</div>
-        <div class="footer-fixed-info-btn">去领奖</div>
+        <div class="footer-fixed-info-btn" @click="addressShow = true">去领奖</div>
       </div>
     </footer>
+
+    <van-popup v-model:show="addressShow" position="bottom" :style="{ height: '43%' }">
+      <div class="address">
+        <div class="address-title">完善物流信息</div>
+        <van-cell-group inset :border="false">
+          <van-field label="收货人" label-width="60" placeholder="请输入收货人" />
+          <van-field type="tel" label="手机号码" label-width="60" placeholder="请输入手机号码" />
+          <van-field v-model="result" label-width="60" readonly name="area" label="地区选择" placeholder="点击选择省市区" @click="showArea = true" />
+          <van-popup v-model:show="showArea" position="bottom">
+            <van-area :area-list="areaList" @confirm="onConfirm" @cancel="showArea = false" />
+          </van-popup>
+          <van-field rows="3" label-width="60" autosize label="详细地址" type="textarea" placeholder="街道小区" />
+        </van-cell-group>
+        <div class="address-btn"></div>
+      </div>
+    </van-popup>
   </div>
 </template>
 
-<script lang="ts">
-import { Image as VanImage, Overlay } from 'vant'
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { areaList } from '@vant/area-data'
+const addressShow = ref(false)
+const showArea = ref(false)
+const result = ref('')
+const onConfirm = (areaValues: any[]) => {
+  showArea.value = false
+  result.value = areaValues
+    .filter((item: any) => Boolean(item))
+    .map((item: { name: any }) => item.name)
+    .join('/')
+}
+</script>
 
+<script lang="ts">
+import { Popup, Field, CellGroup, Area } from 'vant'
+import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'RankingList',
   components: {
-    [VanImage.name]: VanImage,
-    [Overlay.name]: Overlay
+    [Field.name]: Field,
+    [CellGroup.name]: CellGroup,
+    [Popup.name]: Popup,
+    [Area.name]: Area
   }
 })
 </script>
